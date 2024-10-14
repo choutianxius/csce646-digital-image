2ef26386b34765416938d8b5da08fcc8ab9bba63
choutianxius <choutianxius@gmail.com>
2024-10-13 21:56:59 -0500

    Use Tailwind Typography for styling; finish contents

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindTypography from "@tailwindcss/typography";
+
 export default {
   content: ["./index.html", "./pr01/index.html"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [tailwindTypography],
 };
