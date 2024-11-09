d22866d17b07554aa6b448eabd4d5f0508e2a7c6
Tianxiu Zhou <choutianxius@gmail.com>
2024-11-09 14:15:25 -0600

    Project 2

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, "index.html"),
         pr01: resolve(__dirname, "pr01/index.html"),
+        pr02: resolve(__dirname, "pr02/index.html"),
       },
     },
   },
